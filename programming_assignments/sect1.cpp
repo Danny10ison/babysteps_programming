f60283aeaa1a0c271dfe5d4a77acf3a3f18e1571
dannyose <dannyose@rootkit.dxs>
2021-05-28 01:25:31 +0100

    add challenge1 -> sect1.cpp

---
+++ programming_assignments/sect1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// This code is expected to display [ Success Loves Preparation ]
+  // But it doesnt work
+int mian()[
+
+    string quote = "Success Loves Preparation";
+
+    cout << "Today's quote is " << quote <<endl;
+
+    return 0;
+]
